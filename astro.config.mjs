f2a95066b1a25adbfb1a4569c98c37d06effbe5a
Adrianfeloh1 <adrianfeloh1@gmail.com>
2024-10-06 20:40:27 -0500

    Iniciando About

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  experimental: {
-    directRenderScript: true,
-  },
 });
